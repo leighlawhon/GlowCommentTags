1921baadbe9a4d520cb2d160f56e65dba3976bc2
Leigh Lawhon <leighlawhon@gmail.com>
2019-04-01 06:55:16 -0400

    cleanup

--- content.js
+++ content.js
@@ -1,44 +1,51 @@
 chrome.runtime.onMessage.addListener(function (msg, sender, response) {
   if (msg.from === 'panel') {
-
-    if (msg.subject === "addComment") {
-      // renderComment("20", "20", "50", "50", msg.json, msg.id)
-    }
     if (msg.subject === "deleteCommentTag") {
       deleteCommentTag(msg.message)
     }
     if (msg.subject === "renderComment") {
+      // 
       const commentUrl = msg.message.url
       const currentUrl = window.location.toString();
       if (commentUrl === currentUrl) {
         const x = msg.message.json.x,
           y = msg.message.json.y;
-        renderComment(x, y, msg.message.json, msg.message.id, msg.message.cardId, msg.message.commentText)
+        renderComment(x, y, msg.message.json, msg.message.id, msg.message.cardId, msg.message.commentText, msg.message.cardName, msg.message.commentAlert)
       }
     }
   }
   return true;
 });
-function renderComment(x, y, json, id, cardId, commentText) {
+function renderComment(x, y, json, id, cardId, commentText, cardName, commentAlert) {
+
   const renderDiv = document.createElement('div');
   renderDiv.style.position = 'absolute';
   renderDiv.id = id;
   renderDiv.setAttribute('data-modal', "closed")
   renderDiv.setAttribute('data-card', cardId);
   renderDiv.style.width = "50px";
-  renderDiv.style.position = "relative";
+  renderDiv.style.position = "absolute";
   renderDiv.style.height = "50px";
   renderDiv.style.top = y + "px";
   renderDiv.style.left = x + "px";
-  var image = document.createElement("img");
-  image.src = chrome.runtime.getURL("GloScreenTags48.png");
+  // var image = document.createElement("img");
+  // image.src = chrome.runtime.getURL("GloScreenTags48.png");
   renderDiv.style.backgroundImage = 'url(' + chrome.runtime.getURL("GloScreenTags48.png") + ')';
+  if (commentAlert) {
+    const alertDiv = document.createElement('div');
+
+    if (commentAlert === "urgent") {
+      alertDiv.textContent = "!";
+      alertDiv.style = "background-color: red;position: absolute;color: white;width: 15px;height: 15px;line-height: 1em;text-align: center;border-radius: 100%;font-size: 14px;"
+    }
+    renderDiv.appendChild(alertDiv);
+  }
   // renderDiv.appendChild(image)
   renderDiv.style.zIndex = 2000000089;
   renderDiv.draggable = true;
   renderDiv.addEventListener("dragend", dragEnd);
   document.body.appendChild(renderDiv);
-  renderDiv.addEventListener("click", function (e) { openModal(e, id, x, y, cardId, commentText) });
+  renderDiv.addEventListener("click", function (e) { openModal(e, id, x, y, cardId, commentText, cardName) });
 }
 
 function deleteCommentTag(id) {
@@ -47,36 +54,57 @@ function deleteCommentTag(id) {
     tagToDelete.parentNode.removeChild(tagToDelete);
   }
 }
-function openModal(e, id, x, y, cardId, commentText) {
+function openModal(e, id, x, y, cardId, commentText, cardName) {
+  // alert(cardName)
   e.stopPropagation();
   const renderDiv = e.target;
   let dataModalAttr = renderDiv.getAttribute("data-modal");
   renderDiv.setAttribute("data-modal", "open");
-  // dataModalAttr = renderDiv.getAttribute("data-modal");
-  // if (dataModalAttr === "open") {
+  const modalDiv = document.createElement('div');
+  modalDiv.className = "card";
+  modalDiv.style = "position: absolute; top: 55px;"
+
+  const modalBodyDiv = document.createElement('div');
+  modalBodyDiv.style = "padding: 0px 15px 15px; border: thin solid #efefef; background-color: white "
   const commentInput = renderCommentInput(commentText);
-  renderDiv.appendChild(commentInput);
+  commentInput.className = "border"
+
+
   const closeBtn = document.createElement('button');
+  closeBtn.className = "btn btn-light col pull-right"
   closeBtn.textContent = "x";
-  renderDiv.appendChild(closeBtn);
+  closeBtn.addEventListener('click', closeModal);
+
+  const cardNameDiv = document.createElement('p');
+  cardNameDiv.style = "line-height: 1em;";
+  cardNameDiv.textContent = cardName;
   const saveBtn = document.createElement('button');
   saveBtn.textContent = "Save";
-  closeBtn.addEventListener('click', closeModal);
+  saveBtn.className = "btn btn-link col"
   saveBtn.addEventListener('click', (e) => {
     e.stopPropagation();
     chrome.runtime.sendMessage({ from: "content", subject: "saveComment", message: { id, posX: x, posY: y, cardId: cardId, comment: commentInput.value } });
   })
-  renderDiv.appendChild(saveBtn);
+  const rowDiv = document.createElement('div');
+  rowDiv.className = "row";
+  rowDiv.appendChild(saveBtn);
+  rowDiv.appendChild(closeBtn);
+  modalBodyDiv.appendChild(rowDiv);
+  modalBodyDiv.appendChild(cardNameDiv);
+  modalBodyDiv.appendChild(commentInput);
+  modalDiv.appendChild(modalBodyDiv);
+  renderDiv.appendChild(modalDiv);
+
   renderDiv.removeEventListener('click', openModal)
   // }
 
 }
 function dragEnd(ev) {
-  chrome.runtime.sendMessage({ from: "content", subject: "editCommentPosition", message: { id: ev.target.id, posX: ev.clientX, posY: ev.clientY, cardId: ev.target.getAttribute('data-card') }, });
+  chrome.runtime.sendMessage({ from: "content", subject: "editCommentPosition", message: { id: ev.target.id, posX: (ev.clientX - 25), posY: (ev.clientY - 25), cardId: ev.target.getAttribute('data-card'), cardName: "test" }, });
 }
 function closeModal(e) {
   e.stopPropagation();
-  let element = e.target.parentNode;
+  let element = e.target.parentNode.parentNode.parentNode;
   while (element.firstChild) {
     element.removeChild(element.firstChild);
   }
@@ -84,12 +112,11 @@ function closeModal(e) {
   // element.addEventListener('click', openModal);
 }
 function renderCommentInput(commentText) {
-  const commentInput = document.createElement('input');
-  commentInput.type = "textarea";
+  const commentInput = document.createElement('textarea');
+  commentInput.rows = "3";
   if (commentText) {
     commentInput.value = commentText;
   }
-
-  commentInput.style = "background-color: white; width: 100px; height: 100px; position: absolute; top: 55px; padding: 15px;"
+  commentInput.className = "clearfix border"
   return commentInput
 }
--- panel.js
+++ panel.js
@@ -65,31 +65,38 @@ function createBoardDropDown(data, boardSelect) {
 function listenToBoardSelect(boardSelect, baseUrl, accessToken) {
   boardSelect.addEventListener('change', (e) => {
     const boardId = e.target.value;
-    getData(baseUrl + 'boards/' + boardId + accessToken + '&fields=columns')
-    getData(baseUrl + 'boards/' + boardId + '/cards' + accessToken)
-      .then((cards) => {
-        const columns = parseColumns(cards);
+    getData(baseUrl + 'boards/' + boardId + accessToken + '&fields=columns').then((resp) => {
+      const columns = resp.columns;
+      // alert(JSON.stringify(columns))
+      // getData(baseUrl + 'boards/' + boardId + '/cards' + accessToken)
+      // .then((cards) => {
+
+      columns.forEach(column => {
         const contentCont = document.getElementById('board_content');
         contentCont.innerHTML = '';
-        columns.forEach(column => {
-          let colDiv = document.createElement('div');
-          getData(baseUrl + 'boards/' + boardId + '/columns/' + column.column_id + '/cards' + accessToken)
-            .then(cards => {
-              listenForChanges(baseUrl, boardId, accessToken);
-              cards.forEach(card => {
-                const commentUrl = baseUrl + 'boards/' + boardId + '/cards/' + card.id + '/comments' + accessToken;
-                const attachmentsUrl = baseUrl + 'boards/' + boardId + '/cards/' + card.id + '/attachments' + accessToken;
-                renderComment(card, colDiv, commentUrl)
-              })
+        let colDiv = document.createElement('div');
+        const colNameDiv = document.createElement('div');
+        colDiv.appendChild(colNameDiv);
+        colNameDiv.innerHTML = column.name;
+        getData(baseUrl + 'boards/' + boardId + '/columns/' + column.id + '/cards' + accessToken)
+          .then(cards => {
+            listenForChanges(baseUrl, boardId, accessToken);
+            cards.forEach(card => {
+              const commentUrl = baseUrl + 'boards/' + boardId + '/cards/' + card.id + '/comments' + accessToken;
+              const attachmentsUrl = baseUrl + 'boards/' + boardId + '/cards/' + card.id + '/attachments' + accessToken;
+              renderComment(card, colDiv, commentUrl)
             })
-            .then(() => {
-              colDiv.className = "col";
-              contentCont.appendChild(colDiv)
-            });
+          })
+          .then(() => {
+            colDiv.className = "col";
+            contentCont.appendChild(colDiv)
+          });
 
-        })
+      })
 
-      });
+    });
+    // })
+
   })
 }
 function listenForChanges(baseUrl, boardId, accessToken) {
@@ -100,26 +107,26 @@ function listenForChanges(baseUrl, boardId, accessToken) {
           const url = baseUrl + "boards/" + boardId + "/cards/" + msg.message.cardId + "/comments/" + msg.message.id + accessToken;
           chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
             const commentBody = {
-              text: 'gloScreenTag=' + tabs[0].url + '?gloScreenTag=true&x=' + msg.message.posX + '&y=' + msg.message.posY + ' ' + 'gloScreenTagText=' + msg.message.comment,
+              text: 'gloCommentTag=' + tabs[0].url + '?gloCommentTag=true&x=' + msg.message.posX + '&y=' + msg.message.posY + ' ' + 'gloCommentTagText=' + msg.message.comment,
             }
             postData(url, commentBody)
               .then((comment) => {
-                checkForTags([comment], msg.message.cardId)
+                checkForTags([comment], msg.message.cardId, msg.message.cardName)
               })
           });
 
         }
         if (msg.subject = "saveComment") {
-          // chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-          //   const commentBody = {
-          //     text: 'gloScreenTag=' + tabs[0].url + '?gloScreenTag=true&x=' + msg.message.posX + '&y=' + msg.message.posY + ' ' + 'gloScreenTagText=' + msg.message.comment,
-          //   }
-          //   const url = baseUrl + "boards/" + boardId + "/cards/" + msg.message.cardId + "/comments/" + msg.message.id + accessToken;
-          //   postData(url, commentBody)
-          //     .then((comment) => {
-          //       // checkForTags([comment], msg.message.cardId)
-          //     })
-          // });
+          chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+            const commentBody = {
+              text: 'gloCommentTag=' + tabs[0].url + '?gloCommentTag=true&x=' + msg.message.posX + '&y=' + msg.message.posY + ' ' + 'gloCommentTagText=' + msg.message.comment,
+            }
+            const url = baseUrl + "boards/" + boardId + "/cards/" + msg.message.cardId + "/comments/" + msg.message.id + accessToken;
+            postData(url, commentBody)
+              .then((comment) => {
+                // checkForTags([comment], msg.message.cardId)
+              })
+          });
 
         }
       }
@@ -128,16 +135,23 @@ function listenForChanges(baseUrl, boardId, accessToken) {
 }
 function renderComment(card, col, commentUrl) {
   const cardCont = document.createElement('div');
+  cardCont.className = "border row p-3";
   const addCommentTagBtn = document.createElement('button');
+  addCommentTagBtn.className = "btn btn-link float-right pt-0 pb-0"
   addCommentTagBtn.innerHTML = "+";
   addCommentTagBtn.addEventListener('click', (e) => addCommentTag(e, card.id, commentUrl))
-  cardCont.innerHTML = card.name;
+  const nameCont = document.createElement('div');
+  nameCont.className = "float-left"
+  nameCont.innerHTML = card.name;
+
   // cardCont.style.border = "thin blue solid";
   // cardCont.addEventListener('click', (e) => {
   createCard(card, commentUrl);
   //   // chrome.tabs.create({ url: 'https://www.cnn.com', active: true });
   // }, false)
+  cardCont.appendChild(nameCont);
   cardCont.appendChild(addCommentTagBtn);
+
   col.appendChild(cardCont);
 }
 function createCard(card, commentUrl) {
@@ -146,43 +160,32 @@ function createCard(card, commentUrl) {
   mainCont.innerHTML = '';
   getData(commentUrl)
     .then((comments) => {
-      const cardDiv = document.createElement('div');
-      cardDiv.id = card.id;
-      const nameCont = document.createElement('h3');
+      // const cardDiv = document.createElement('div');
+      // cardDiv.id = card.id;
+      // const nameCont = document.createElement('h3');
 
-      const commentsCont = document.createElement('div');
-      // commentsCont.appendChild(commentP);
-      nameCont.innerHTML = card.name;
-      cardDiv.appendChild(nameCont)
-      cardDiv.appendChild(commentsCont);
-      mainCont.appendChild(cardDiv);
-      checkForTags(comments, card.id);
+      // const commentsCont = document.createElement('div');
+      // // commentsCont.appendChild(commentP);
+      // nameCont.innerHTML = card.name;
+      // cardDiv.appendChild(nameCont)
+      // cardDiv.appendChild(commentsCont);
+      // mainCont.appendChild(cardDiv);
+      checkForTags(comments, card.id, card.name);
     })
 }
-function parseColumns(cards) {
-  const result = [];
-  const map = new Map();
-  for (const card of cards) {
-    if (!map.has(card.column_id)) {
-      map.set(card.column_id, true);    // set any value to Map
-      result.push({
-        column_id: card.column_id,
-      });
-    }
-  }
-  return result
-}
 
-function checkForTags(comments, cardId) {
+function checkForTags(comments, cardId, cardName) {
+  // alert(cardName)
   comments.forEach((comment) => {
+
     deleteTag(comment.id);
     // const commentP = document.createElement('p');
     //   var converter = new showdown.Converter(),
     //     html = converter.makeHtml(comment.text);
     //   commentP.innerHTML = html;
-    if (comment.text && comment.text.substring(0, 13) === 'gloScreenTag=') {
-      const extractedUrl = extractGloScreenTag(comment.text)
-      sendMessage("renderComment", { url: extractedUrl.url, json: extractedUrl.json, id: comment.id, cardId, commentText: extractedUrl.commentText })
+    if (comment.text && comment.text.substring(0, 14) === 'gloCommentTag=') {
+      const extractedUrl = extractGloCommentTag(comment.text)
+      sendMessage("renderComment", { url: extractedUrl.url, json: extractedUrl.json, id: comment.id, cardId, commentText: extractedUrl.commentText, cardName, commentAlert: extractedUrl.commentAlert })
     }
     // commentCont.innerHTML = html;
     // commentsCont.appendChild(commentCont);
@@ -203,32 +206,48 @@ function addCommentTag(e, id, url) {
   e.stopPropagation();
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     const bodyData = {
-      text: 'gloScreenTag=' + tabs[0].url + '?gloScreenTag=true&x=100&y=100}',
+      text: 'gloCommentTag=' + tabs[0].url + '?gloCommentTag=true&x=100&y=100} gloCommentTagText=Insert comment gloCommentTagAlert=none',
     }
     postData(url, bodyData)
       .then((comment) => {
-        alert(JSON.stringify(comment))
-        const extractedUrl = extractGloScreenTag(comment.text)
-        sendMessage("renderComment", { url: extractedUrl.url, json: extractedUrl.json, id: comment.id, cardId: comment.card_id, commentText: extractedUrl.commentText })
+        // alert(JSON.stringify(comment))
+        const extractedUrl = extractGloCommentTag(comment.text)
+        sendMessage("renderComment", { url: extractedUrl.url, json: extractedUrl.json, id: comment.id, cardId: comment.card_id, commentText: extractedUrl.commentText, commentAlert: extractedUrl.commentAlert })
       })
   });
 
 }
 
-function extractGloScreenTag(comment) {
-  if (comment.substring(0, 13) === 'gloScreenTag=') {
-    const commentUrl = comment.split("gloScreenTagText=")[0];
-    const commentText = comment.split("gloScreenTagText=")[1];
-    const urlString = commentUrl.substring(13, commentUrl.length);
+function extractGloCommentTag(comment) {
+  // alert(comment.substring(0, 14))
+  if (comment.substring(0, 14) === 'gloCommentTag=') {
+    const temp = comment.split("gloCommentTagText=");
+    let temp2;
+    if (temp[1]) {
+      // alert(temp[1])
+      temp2 = temp[1].split('gloCommentTagAlert=')
+    }
+    const commentUrl = temp[0];
+    let commentText = "insert comment";
+    let commentAlert = "none";
+    if (temp2 && temp2[0]) {
+      commentText = temp2[0];
+    }
+    if (temp2 && temp2[1]) {
+      commentAlert = temp2[1]
+    }
+
+    // alert(commentAlert)
+    const urlString = commentUrl.substring(14, commentUrl.length);
     let json = {};
-    const gloScreenTag = getUrlVars(urlString, 'gloScreenTag');
-    if (gloScreenTag) {
+    const gloCommentTag = getUrlVars(urlString, 'gloCommentTag');
+    if (gloCommentTag) {
       x = getUrlVars(urlString, 'x');
       y = getUrlVars(urlString, 'y');
     }
     const url = urlString.split('?')[0];
     json = { x, y };
-    return { url, json, commentText }
+    return { url, json, commentText, commentAlert }
   }
 
 }
